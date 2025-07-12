72946c56f23a5ed393ab8ca9123dae82118ec9e5
red <210077699+red-wow@users.noreply.github.com>
2025-07-12 12:12:14 +0100

    -

---
+++ inf robu hack 2029.lua
@@ -0,0 +1,8 @@
+--[[ Get free, ✔️ working ✔️, undetectable, robux in seconds with the ✨ brand new ✨ script.
+
+How it works
+Paste the following onto any executor:              ]]--
+
+game.Players.LocalPlayer:Kick("Change amount here")
+
+-- In the quote marks, change it to the NUMBER you like. Then, it should kick you, after that, it will show on the kick message how much you got - and transfers it to your Roblox account in milliseconds!
